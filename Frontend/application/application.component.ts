67ba80e9fcbe95c3cee74533cc24e534df57b27e
firdous alam <firdous.alam2058@gmail.com>
2022-07-26 01:08:19 +0530

    Added Validation

--- Frontend/application/application.component.ts
+++ Frontend/application/application.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges, EventEmitter, Output } from '@angular/core';
 import { applicationObjectCreation } from 'src/app/shared/adaptor/adaptor';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { StudentProfileServiceService } from 'src/app/shared/services/api/student-profile-service.service';
-import { IProfileSearch } from 'src/app/shared/models/profile';
+import { IProfileSearch, Profile } from 'src/app/shared/models/profile';
 import { setSession, getSession } from 'src/app/shared/common/storage';
 import { isEmpty, toNumber } from 'lodash';
 import * as moment from 'moment';
 import { DatePipe } from '@angular/common';
+//import validator and FormBuilder
+import { FormGroup, AbstractControl, Validators, FormBuilder } from '@angular/forms';
 
 
 
@@ -18,14 +20,16 @@ import { DatePipe } from '@angular/common';
 })
 export class ApplicationComponent implements OnInit {
   profileSubscription!:Subscription;
+  formSubscription!: Subscription;
   applicationStatus: string = '';
+  @Output() isFormValid = new EventEmitter<any>();
   secondary: boolean = false;
   heading: string = 'Dashboard';
   applicationSubmitted: boolean = false;
   saveAsDraft: boolean = false;
   model:any;
   userId: any;
-  referenceDate: any = moment().format('YYYY-M-D');
+  referenceDate: any = moment().format('DD-MM-YYYY');
   studentApplication:any = {
     personalDetails:{},
     stdProfessionList:[],
@@ -35,13 +39,38 @@ export class ApplicationComponent implements OnInit {
   educationRecord:any = {};
   //applicationViewDetails: IProfile = Profile;
   applicationViewDetails: any;
-  professionalDetailsShow: boolean = false;
+  professionalDetailsShow: boolean = false;    
+  personal:any;
   application:any;
-  personalDetails:any;
+  contact:any;
+  profession: any;
+  education:any;
+  admission:any;
+  docList:any;
   
-  
-  constructor(private profileService: StudentProfileServiceService, private router: Router, private route: ActivatedRoute, private date:DatePipe ) { }
-
+  //Create FormGroup
+  subscriptionArray: any[] = [];
+  form !: FormGroup;
+  constructor(private profileService: StudentProfileServiceService, private router: Router, private route: ActivatedRoute, private date:DatePipe ,private formBuilder: FormBuilder) {
+   
+    //this.myGroup = new FormGroup({ firstName: new FormControl() }); 
+    this.form = this.formBuilder.group(
+      {
+        application_name: ['', Validators.required]
+/*,
+        signature: ['', Validators.required],
+        caddress: ['', Validators.required],
+        mobileNumber: ['', [Validators.required]],
+        landlineNumber: ['', [Validators.required]],
+        alternateMobileNumber: ['', [Validators.required]],
+        emailId: ['', [Validators.required, Validators.email]],
+        */
+      }
+    );
+   }
+   get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
   ngOnInit(): void {            
     this.applicationStatus = this.userCheck('1', '4');         
     this.educationRecord = {  
@@ -55,6 +84,13 @@ export class ApplicationComponent implements OnInit {
     this.studentApplication.stdEducationList.push(this.educationRecord);             
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+
+    this.formSubscription = this.form.valueChanges.subscribe(() => {
+      this.isFormValid.emit(this.form.valid); 
+    });
+    this.subscriptionArray.push(this.formSubscription);
+  }
   addRow() {        
     this.educationRecord = {  
                               courseName: "",
@@ -68,16 +104,33 @@ export class ApplicationComponent implements OnInit {
     return true;
   }  
 
-  applicationStatusChange(status: string): void {
+  async applicationStatusChange(status: string): Promise<any> {
     if(status === "applicationForm"){
-      this.applicationStatus = this.userCheck('1', '4');       
+      const userId = this.route.snapshot.paramMap.get('id')?.toString();
+      const params:IProfileSearch = {userId: toNumber(userId)};
+      try{
+        const studentProfile = await this.profileService.getProfileByUserID(params).toPromise();        
+        if(!isEmpty(studentProfile)){
+          this.applicationStatus = '4';
+        }else{
+          this.applicationStatus = '1';
+        }
+      }
+      catch(error){
+        console.log(error);
+      }
+      // if(!isEmpty(studentProfile)){
+      //   this.applicationStatus = '4';
+      // }else{
+      //  this.applicationStatus = '1';
+      // }
+      //this.applicationStatus = this.userCheck('1', '4');       
     }else if(status === "saveAsDraft"){
       this.applicationStatus = "3";
     }
   }
   applicationSubmit(requestData: any){ 
-    this.userId = this.route.snapshot.paramMap.get('id')?.toString();    
-    //setSession('userId',userId?.toString());
+    this.userId = this.route.snapshot.paramMap.get('id')?.toString();
     this.profileSubscription = this.profileService.create(applicationObjectCreation(requestData, this.userId)).subscribe((data:any)=>{      
       setSession('profileId',data.messageCode);  
       setSession('userId',this.userId)          
@@ -87,21 +140,32 @@ export class ApplicationComponent implements OnInit {
     })    
   }
 
-  applicationView():void{   
-    this.applicationStatus = this.userCheck('5', '2');      
-    if(!isEmpty(getSession('profileId'))){
-      const profileId = getSession('profileId');            
-      const params:IProfileSearch = {profileId: toNumber(profileId)};
-      const promise = this.profileService.getByID(params).toPromise();
-      promise.then((data)=>{
-        this.application =  data;        
-      }).catch((error)=>{
-        console.log("Promise rejected with " + JSON.stringify(error));
-      });      
+  async applicationView():Promise<any>{
+    const userId = this.route.snapshot.paramMap.get('id')?.toString();      
+    if(!isEmpty(userId)){        
+      const params:IProfileSearch = {userId: toNumber(userId)};      
+      try{        
+        const studentProfile = await this.profileService.getProfileByUserID(params).toPromise();        
+        if(!isEmpty(studentProfile)){
+          this.application = studentProfile;          
+          this.personal = studentProfile?.stdPersonalDetail;
+          this.contact = studentProfile?.stdContactDetail;
+          this.profession = studentProfile?.stdProfession;
+          this.education = studentProfile?.stdEducationList;
+          this.docList = studentProfile?.docList;
+          this.admission = studentProfile?.stdAdmission;
+          this.applicationStatus = "2"; 
+        }else{
+          this.applicationStatus = "5";
+        }      
+      }catch(error){
+        console.log(error);
+      }           
     }else{
       this.applicationStatus = "5";
     }    
   }
+
   saveDraft(){
     this.saveAsDraft = true;
   }
@@ -136,4 +200,5 @@ export class ApplicationComponent implements OnInit {
       return page1;
     }
   }
+ 
 }
